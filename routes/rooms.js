d111d8927ef3efc54bece076941e888d12b95ad6
Nolan Kovacik <noltron000@gmail.com>
2018-10-23 16:22:17 -0700

    able to edit & update rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -30,12 +30,18 @@ router.get('/:id', auth.requireLogin, (req, res, next) => {
 
 // Rooms edit
 router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
-	// TODO
+	Room.findById(req.params.id, function (err, room) {
+		if (err) { console.error(err) };
+		res.render('rooms/edit', { room: room });
+	});
 });
 
 // Rooms update
 router.post('/:id', auth.requireLogin, (req, res, next) => {
-	// TODO
+	Room.findByIdAndUpdate(req.params.id, req.body, function (err, room) {
+		if (err) { console.error(err) };
+		res.redirect('/rooms/' + req.params.id);
+	});
 });
 
 // Rooms create
