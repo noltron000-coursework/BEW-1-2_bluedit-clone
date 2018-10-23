5a3a98b9660b8e2e4198588f42ef675eb9f94779
Nolan Kovacik <noltron000@gmail.com>
2018-10-23 15:43:01 -0700

    adding BCrypt to passwords

--- models/user.js
+++ models/user.js
@@ -1,9 +1,24 @@
+// Requirements
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema;
 
+// Other Variables
+const User = mongoose.model('User', UserSchema);
+
 const UserSchema = new Schema({
-	username: { type: String, required: true }
+	username: { type: String, required: true },
+	password: { type: String, required: true }
 });
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.pre('save', function (next) {
+	let user = this;
+	bcrypt.hash(user.password, 10, function (err, hash) {
+		if (err) return next(err);
+		user.password = hash;
+		next();
+	})
+});
+
+// Export
 module.exports = User;
