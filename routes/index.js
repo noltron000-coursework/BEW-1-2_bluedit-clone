a10c74469392ecbfeb9485b97deebfea4efef0fa
Nolan Kovacik <noltron000@gmail.com>
2018-10-23 15:59:56 -0700

    able to logout now

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,45 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-// SHOW homepage
-router.get('/', (req, res, next) => {
-	const currentUserId = req.session.userId;
-	res.render('index', { title: 'Bluedit', currentUserId: currentUserId });
+// set layout variables
+router.use(function (req, res, next) {
+	res.locals.title = "Bluedit";
+	res.locals.currentUserId = req.session.userId;
+	next();
 });
 
+// home page
+router.get('/', (req, res, next) => {
+	res.render('index');
+});
 
-// SHOW login
+// login
 router.get('/login', (req, res, next) => {
 	res.render('login');
 });
 
-// NEW login
+// POST login
 router.post('/login', (req, res, next) => {
 	User.authenticate(req.body.username, req.body.password, (err, user) => {
 		if (err || !user) {
 			const next_error = new Error("Username or password incorrect");
 			next_error.status = 401;
-
 			return next(next_error);
 		} else {
 			req.session.userId = user._id;
-
 			return res.redirect('/');
 		}
 	});
+});
+
+// GET logout
+router.get('/logout', (req, res, next) => {
+	if (req.session) {
+		req.session.destroy((err) => {
+			if (err) return next(err);
+		});
+	}
+	return res.redirect('/login');
 });
 
 module.exports = router;
